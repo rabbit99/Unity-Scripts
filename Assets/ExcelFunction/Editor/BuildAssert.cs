7000d72c725fa1478b6e790faee9a04cea7fa982
vvbest2012 <vvbest2012@gmail.com>
2019-05-22 01:53:09 +0800

    上傳練習讀取/寫入/匯出 excel及ScriptableObject的方法 參考 https://blog.csdn.net/yupu56/article/details/50580277

---
+++ Assets/ExcelFunction/Editor/BuildAssert.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 利用ScriptableObject创建资源文件
+/// </summary>
+public class BuildAsset : Editor {
+
+    [MenuItem("BuildAsset/Build Scriptable Asset")]
+    public static void ExcuteBuild()
+    {
+        BookHolder holder = ScriptableObject.CreateInstance<BookHolder>();
+
+        //查询excel表中数据，赋值给asset文件
+        holder.menus = ExcelAccess.SelectMenuTable();
+
+        string path= "Assets/Resources/booknames.asset";
+
+        AssetDatabase.CreateAsset(holder, path);
+        AssetDatabase.Refresh();
+
+        Debug.Log("BuildAsset Success!");
+    }
+
+    [MenuItem("BuildAsset/Export Excel")]
+    public static void ExportExcel()
+    {
+        string excelName = "william_test.xlsx";
+        string sheetName = "randomTest";
+        string path = Application.dataPath + "/" + excelName;
+        ExcelAccess.WriteExcel(excelName, sheetName, path);
+        
+        AssetDatabase.Refresh();
+
+        Debug.Log("ExportExcel Success!");
+    }
+}
